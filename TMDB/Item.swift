6357b126fdae0eb601e234ea0d297c7e99bea88e
Technical-interview-yassir <>
2023-12-18 22:11:49 +0100

    Project creation

---
+++ TMDB/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TMDB
+//
+//  Created by Maxime Bentin on 18.12.23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
