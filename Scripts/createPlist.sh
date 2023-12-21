42f39698610e946e0272395b12cee8cd0a9f8a56
Technical-interview-yassir <>
2023-12-21 14:06:47 +0100

    Create the empty plist

---
+++ Scripts/createPlist.sh
@@ -0,0 +1,13 @@
+# bash
+
+touch TMDB/Networking/Secrets.plist
+cat << EOF >> TMDB/Networking/Secrets.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0"> 
+<dict>
+<key>TMDB_accessToken</key>
+<string></string>
+</dict>
+</plist>
+EOF
